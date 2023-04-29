329600cdfa2f5cd9176cfa3497139bf02c0b4746
SimonHic <simonhicdev@gmail.com>
2023-04-29 16:43:37 +0100

    Add fix to XML persistence and menu

--- src/main/kotlin/persistence/XXMLSerializer.kt
+++ src/main/kotlin/persistence/XXMLSerializer.kt
@@ -15,7 +15,7 @@ class XMLSerializer(private val file: File) : Serializer {
     @Throws(Exception::class)
     override fun read(): Any {
         val xStream = XStream(DomDriver())
-        xStream.allowTypes(arrayOf(Note::class.java))
+        xStream.allowTypes(arrayOf(Note::class.java, Item::class.java))
         val inputStream = xStream.createObjectInputStream(FileReader(file))
         val obj = inputStream.readObject() as Any
         inputStream.close()
